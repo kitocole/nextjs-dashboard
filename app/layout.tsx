58a3305969a41f8f60dbcd37e139e9450681950d
kitocole <kaenyitocole@gmail.com>
2025-04-27 19:15:32 -0700

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,16 @@ import './globals.css';
 import { Navbar } from '@/components/layout/Navbar';
 import { SidebarResizeHandler } from '@/components/layout/SidebarResizeHandler';
 import { Providers } from './providers';
-
 export const metadata = {
   title: 'Sandbox App',
   description: 'Kaenys Sandbox App',
   charSet: 'utf-8',
+  icons: {
+    icon: '/assets/favicon.svg', // <link rel="icon">
+    shortcut: '/assets/favicon.svg', // <link rel="shortcut icon">
+    apple: '/assets/favicon.svg', // <link rel="apple-touch-icon">
+  },
 };
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="h-full" suppressHydrationWarning>
